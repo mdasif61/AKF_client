3dfc815071113662a8b8c9b09bc01c97f215a0fc
Ikbal Hossain Asif <asifikbalgfh312@gamil.com>
2023-07-06 19:21:22 +0600

    add icon

--- src/components/Layout/SideNav.jsx
+++ src/components/Layout/SideNav.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Css/SideNav.css";
 import { Link, NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClockRotateLeft, faGear, faHouseSignal, faMoneyCheck, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const SideNav = ({ children }) => {
   return (
@@ -11,7 +13,7 @@ const SideNav = ({ children }) => {
             to="/dashboard/diposite"
             className={({ isActive }) => (isActive ? "manu-bg" : "manu-p")}
           >
-            Diposite Now
+            <FontAwesomeIcon icon={faMoneyCheck}/> Diposite Now
           </NavLink>
         </p>
         <p>
@@ -19,26 +21,26 @@ const SideNav = ({ children }) => {
             to="/dashboard/history"
             className={({ isActive }) => (isActive ? "manu-bg" : "manu-p")}
           >
-            Diposite History
+          <FontAwesomeIcon icon={faClockRotateLeft}/>  Diposite History
           </NavLink>
         </p>
         <p>
           <NavLink to='/dashboard/settings'
             className={({ isActive }) => (isActive ? "manu-bg" : "manu-p")}
           >
-            Settings
+          <FontAwesomeIcon icon={faGear}/>  Settings
           </NavLink>
         </p>
         <p>
           <NavLink to='/dashboard/profile'
             className={({ isActive }) => (isActive ? "manu-bg" : "manu-p")}
           >
-            Profile
+          <FontAwesomeIcon icon={faUser}/>  Profile
           </NavLink>
         </p>
         <p>
           <Link className="manu-p" to='/'>
-          <button className=" text-black">Back To Home</button>
+          <FontAwesomeIcon icon={faHouseSignal}/>  <button className=" text-black">Back To Home</button>
           </Link>
         </p>
       </div>
